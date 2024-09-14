aca1008c647cf9304e0e48967fad373752c59bda
diegoandcontroll <diegoandcontroll>
2024-09-14 00:39:36 -0300

    auth: add jwt anotation

--- github-waveapi/src/users/users.controller.ts
+++ github-waveapi/src/users/users.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Request } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
