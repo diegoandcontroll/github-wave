4d5fd780baf8176de4a92760abd6e412afbcc7a0
diegoandcontroll <diegoandcontroll>
2024-09-16 02:07:19 -0300

    fix: adjutment endpoint repos to queryparam

--- github-waveapi/src/github/github.controller.ts
+++ github-waveapi/src/github/github.controller.ts
@@ -15,9 +15,9 @@ export class GithubController {
 
   @Public()
   // Endpoint para obter os repositórios do usuário com paginação
-  @Get('/repos:username')
+  @Get('/repos')
   async getUserRepos(
-    @Param('username') username: string,
+    @Query('username') username: string,
     @Query('page') page: number = 1,
     @Query('perPage') perPage: number = 30,
   ) {
